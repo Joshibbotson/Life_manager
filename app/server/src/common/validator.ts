bd942cf7c469197d4a6fe24d17d79729aafed970
joshibbotson <joshua_ibbotson@hotmail.com>
2023-12-09 20:13:45 +0000

    BUILD - updated config and scripts for deploy

--- app/server/src/common/validator.ts
+++ app/server/src/common/validator.ts
@@ -1,22 +1,22 @@
 // api/validation.middleware.ts
 import { Request, Response, Next } from 'express'
-import { ValidationService } from './validation.service'
+// import { ValidationService } from './validation.service'
 
 export function validateSchemaMiddleware(schemaClass: any) {
   return async (req: Request, res: Response, next: Next) => {
     try {
       const schemaInstance = new schemaClass(req.body)
-      const validationService = new ValidationService()
+    //   const validationService = new ValidationService()
       const validationErrors =
-        await validationService.validateSchema(schemaInstance)
+        // await validationService.validateSchema(schemaInstance)
 
-      if (validationErrors) {
-        res.status(400)
-        res.send({ errors: validationErrors })
-      } else {
-        req.body = schemaInstance
-        next()
-      }
+    //   if (validationErrors) {
+    //     res.status(400)
+    //     res.send({ errors: validationErrors })
+    //   } else {
+    //     req.body = schemaInstance
+    //     next()
+    //   }
     } catch (error) {
       res.status(500)
       res.send({ message: 'Internal Server Error' })
